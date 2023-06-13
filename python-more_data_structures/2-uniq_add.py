cc1157605672168f462fb2e8710c9d2e79a4d1fe
jGohan-cpu <jjoseriveram01@gmail.com>
2023-06-13 15:24:49 -0400

    actualized

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+def uniq_add(my_list=[]):
+    unique_nums = set()
+    total = 0
+    for num in my_list:
+        if num not in unique_nums:
+            total += num
+            unique_nums.add(num)
+    return total
---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    uncommon = []
+    for i in set_1:
+        for j in set_2:
+            if :
+                common[] = 
