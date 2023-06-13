8143a9742cdc84d3d888108445c7ae7281a80622
jGohan-cpu <jjoseriveram01@gmail.com>
2023-06-12 20:58:37 -0400

    6

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = []
-    for i in my_list:
-        result.append(i % 2 == 0)
-    return result
+    return [i % 2 == 0 for i in my_list]
