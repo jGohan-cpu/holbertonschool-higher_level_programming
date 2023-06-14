f894c1add8b86d6d275ba0453a6a33da38feffe6
jGohan-cpu <jjoseriveram01@gmail.com>
2023-06-13 20:17:22 -0400

    0th

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,12 @@
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+#!/usr/bin/python3
+def square_matrix_simple(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    result = [[0] * cols for _ in range(rows)]
+
+    for i in range(rows):
+        for j in range(cols):
+            result[i][j] = matrix[i][j] ** 2
+
+    return result
