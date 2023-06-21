83a1edaed867ded42016c1f65c93d2e2a548702e
jGohan-cpu <jjoseriveram01@gmail.com>
2023-06-21 17:07:22 -0400

    rectangle 2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -59,17 +59,17 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-        def area(self):
-            """
-            Returns area self
-            """
-            return self.__width * self.__height
+    def area(self):
+        """
+        Returns the area of the Rectangle.
+        """
+        return self.__width * self.__height
 
-        def perimeter(self):
-            """
-            returns rectangle checks if perimeter is 0, if so return empty 
-            string
-            """
-            if self.width == 0 or self.height == 0:
-                return 0
-            return 2 * (self.__width + self.__heihgt)
+    def perimeter(self):
+        """
+        Returns the perimeter of the Rectangle.
+        Returns 0 if width or height is 0.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
