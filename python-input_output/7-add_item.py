597356d37eec40d0c187475cba57b436986a0e25
jGohan-cpu <jjoseriveram01@gmail.com>
2023-06-22 21:05:11 -0400

    10 12 file fixed

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,48 @@
-#!/usr/bin/python3
-"""
-This module contains a function that
-add all arguments to a Python list
-and save them to a file.
-"""
 import sys
+import json
+
+# Function to save Python object as JSON to a file
 
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+def save_to_json_file(my_obj, filename):
+    """
+    Save a Python object as JSON to a file.
 
+    Args:
+        my_obj (object): The object to be saved as JSON.
+        filename (str): The name of the file to save the JSON data to.
+    """
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
+
+# Function to load JSON data from a file and convert it to a Python object
+
+
+def load_from_json_file(filename):
+    """
+    Load JSON data from a file and convert it to a Python object.
+
+    Args:
+        filename (str): The name of the file to load JSON data from.
+
+    Returns:
+        object: The Python object created from the JSON data.
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+
+args = sys.argv[1:]
 
 try:
-    js_list = load_from_json_file("add_item.json")
-except Exception:
-    js_list = []
+    # Load existing list from file if it exists
+    existing_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    # If the file doesn't exist, start with an empty list
+    existing_list = []
 
-for arg in sys.argv[1:]:
-    js_list.append(arg)
-save_to_json_file(js_list, "add_item.json")
+# Add the new arguments to the existing list
+existing_list.extend(args)
+
+# Save the updated list to the file
+save_to_json_file(existing_list, 'add_item.json')
