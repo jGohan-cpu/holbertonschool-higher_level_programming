0711cd8a1e6c200937177e36b8a650fb7e1415a5
jGohan-cpu <jjoseriveram01@gmail.com>
2023-06-27 17:33:09 -0400

    commit this files

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+represents rectangle
+'''
+
+
+class Rectangle:
+    '''
+    width and height represented
+    '''
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.width
+
+    @width.setter
+    
+    @property
+
+
+    @height.setter
+
+
